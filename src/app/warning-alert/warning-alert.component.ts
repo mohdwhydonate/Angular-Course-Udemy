5898742ce1486980c0a06f9043abba7502c2998c
MohdSaad <mohd@whydonate.com>
2023-07-06 16:58:04 +0530

    Assignment 2

---
+++ src/app/warning-alert/warning-alert.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-warning-alert',
+  templateUrl: './warning-alert.component.html',
+  styleUrls: ['./warning-alert.component.css']
+})
+export class WarningAlertComponent implements OnInit{
+allowNewServer=false;
+serverCreationStatus=' No Server was created!'
+  userName='';
+
+constructor(){
+  setTimeout(()=>{
+    this.allowNewServer=true;
+  },2000);
+}
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+onCreateServer(){
+  // this.serverCreationStatus="server was created!Name of the server is "+ this.serverName;
+}
+onUpdateServerName(event: any){
+  // this.serverName= event.target.value;
+}
+
+}
